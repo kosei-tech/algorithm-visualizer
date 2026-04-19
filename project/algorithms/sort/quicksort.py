17eb589f56470c6fb821080dc58382ad159dfc22
hotta kosei <kou@hottanonotobukkukonpyuta.local>
2026-04-19 15:20:02 +0900

    ソートの抽象クラスの実装

---
+++ project/algorithms/sort/quicksort.py
@@ -0,0 +1,6 @@
+from .sorter import Sorter
+class quick_sort(Sorter):
+    def partiton(self,data:list[int])->int:
+        
+    def quick_sort(self,data:list[int])->None:
+        
---
+++ project/algorithms/sort/sorter.py
@@ -0,0 +1,4 @@
+# algorithms/sort/sorter.py
+class Sorter:
+    def sort(self, data: list[int]) -> list[int]:
+        raise NotImplementedError()
