3dda768630af0461b0120d446dbd3285a0d5fe4b
hotta kosei <kou@hottanonotobukkukonpyuta.local>
2026-04-20 20:21:23 +0900

    メイン処理

--- project/main.py
+++ project/main.py
@@ -1,6 +1,7 @@
 from algorithms.sort.quicksort import QuickSort
 from algorithms.sort.mergesort import MergeSort
 from services.algorithm_service import SortService
+import time
 
 data = input("データを入力してください: ").split()
 data = list(map(lambda v1:int(v1),data))
@@ -9,5 +10,8 @@ algorithm = algorithm.lower()
 
 service = SortService()
 print(f"ソート前: {data}")
+start = time.time()
 data = service.sort(data,algorithm)
+end = time.time()
 print(f'ソート後: {data}')
+print(f'実行時間{end-start}秒')
