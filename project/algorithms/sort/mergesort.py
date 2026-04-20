0512774470ce952b054e4ca2f695dcfb4b7f72a9
hotta kosei <kou@hottanonotobukkukonpyuta.local>
2026-04-20 13:24:19 +0900

    Fix MergeSort merge

--- project/algorithms/sort/mergesort.py
+++ project/algorithms/sort/mergesort.py
@@ -1,33 +1,41 @@
 from .sorter import Sorter
 class MergeSort(Sorter):
-    def sort(self,data:list[int],p:int,r:int)->list[int]:
-        def merge(self,data:list[int],p:int,q:int,r:int)->None:
+    def merge(self,data:list[int],p:int,q:int,r:int)->None:
             i = p
             j = q+1
-            k = p
             sub_data = []
-            while i <= q or j <= r:
+            while i <= q and j <= r:
                 if data[i] <= data[j]:
-                    sub_data[k].append(data[i])
+                    sub_data.append(data[i])
                     i += 1
                     
                 else: 
-                    sub_data[k].append(data[j])
+                    sub_data.append(data[j])
                     j += 1
                 k += 1
             
             if i <= q:
-                for index in range(p,q):
-                    sub_data[index].appned(data[index])
-                    
-            elif j <= r:
-                for index in range(q+1,r):
+                for index in range(i,q+1):
                     sub_data[index].append(data[index])
                     
-            data = sub_data.copy()
-
+            elif j <= r:
+                for index in range(j,r):
+                    sub_data[index].append(data[index])
+                    
+            data[p:r+1] = sub_data.copy()
+            
+    def sort(self,data:list[int])->list[int]:
+        p = 0
+        if len(data) <= 1:
+            return data.copy()
+        r = len(data)-1
+        sub_data = data.copy() 
+        self._merge_sort(sub_data,p,r)
+        return sub_data
+            
+    def _merge_sort(self,data:list[int],p:int,r:int)->None:
         if p < r:
-            q = (p+r)/2
-            merge_sort(data,p,q)
-            merge_sort(data,q+1,r)
-            merge(data,p,q,r)
+            q = (p+r)//2
+            self._merge_sort(data,p,q)
+            self._merge_sort(data,q+1,r)
+            self.merge(data,p,q,r)
